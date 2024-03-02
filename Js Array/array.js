905cb81a8763d3a2c37c7b0cfbfaa663870f30ab
Moniruzzaman <moniruzzamanm734@gmail.com>
2024-03-02 20:45:56 +0600

    fixed

--- Js Array/array.js
+++ Js Array/array.js
@@ -1,9 +1,9 @@
 const my_array = [7, 12, 9, 41, 11, 2];
 let minValue = my_array[0];
 
-for (let i = 0; i < my_array.length; i++){
-    if (my_array[i] < minValue){
-        minValue = my_array[i]
-    }
+for (let i = 0; i < my_array.length; i++) {
+  if (my_array[i] < minValue) {
+    minValue = my_array[i];
+  }
 }
-console.log(minValue)
+console.log(minValue);
