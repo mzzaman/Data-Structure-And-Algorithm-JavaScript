132651bd01547cbb57077d0776eec020fd632451
Moniruzzaman <moniruzzamanm734@gmail.com>
2024-03-13 23:19:30 +0600

    Added Singly Linked List

---
+++ Singly Linked List/Singly_Linked_List.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Singly_Linked_List {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+}
+
+/*
+let first = new Node("hi");
+console.log(first)
+first.next = new Node("there");
+console.log(first)
+first.next.next = new Node("Hello there");
+console.log(first)
+
+ */
